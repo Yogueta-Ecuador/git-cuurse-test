3a5065aeaf0fa802d758c92cbcd37f60f7a38adc
Bryan <bryan.velasquez@udla.edu.ec>
2020-05-03 00:48:11 -0500

    mi primer commit

---
+++ js/juego.js
@@ -0,0 +1,14 @@
+var vida, ataque, pocion;
+var nombre, textoBienvenida;
+
+nombre='Bryan';
+textoBienvenida = 'Hola '+nombre;
+
+vida = 100;
+ataque = 10;
+pocion = 20;
+
+vida++;
+
+console.log(textoBienvenida);
+console.log(vida);
